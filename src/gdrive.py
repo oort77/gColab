ec0c9e3ad3bb8c16cef36b847d40c63be1169a24
oort77 <73858914+oort77@users.noreply.github.com>
2022-01-30 02:48:11 +0300

    initial commit

---
+++ src/gdrive.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#  File: gdrive.py
+#  Project: 'gColab'
+#  Created by Gennady Matveev (gm@og.ly) on 30-01-2022.
+#  Copyright 2022. All rights reserved.
+
+# %%
+# Import libraries
+import pandas as pd
+# import gdown
+import os
+# import sys
+import os.path
+import tomli
+import pyperclip as pc
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+# %%
+# Import secrets
+secrets_toml = "/Users/gm/Documents/python_scripts/gdrive/config/.secrets.toml"
+with open(secrets_toml, "rb") as f:
+    secrets = tomli.load(f)
+# %%
+# Authorize with gDrive
+gauth = GoogleAuth(settings_file=secrets["settings_file"])
+GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = secrets["secrets_file"]
+GoogleAuth.DEFAULT_SETTINGS['save_credentials_file'] = secrets["save_credentials_file"]
+drive = GoogleDrive(gauth)
+
+# Dataset archives live in downloads:
+work_dir = secrets["work_dir"]
+# datasets folder in Google Drive
+folder_id = secrets["folder_id"]
+# %%
+
+
+# Get list of zip files
+def file_info(dir_name: str):
+    file_list = {}
+    for i in os.listdir(dir_name):
+        if secrets["file_type"] in i:
+            a = os.stat(os.path.join(dir_name, i))
+            file_list[i] = a.st_ctime
+    return file_list
+
+
+# Get last zip
+def get_last_zip(dir_name: str):
+    # Get all
+    zips = file_info(dir_name)
+    # Get the last one
+    return max(zips, key=zips.get)
+
+
+# Send last zip to gDrive
+def send_zip(dir_name: str):
+    last_zip = get_last_zip(dir_name)
+    gfile = drive.CreateFile({'parents': [{'id': folder_id}]})
+    # Read file and set it as the content of this instance
+    gfile.SetContentFile(last_zip)
+    gfile.Upload()  # Upload the file
+    permission = gfile.InsertPermission({
+                                        'type': 'anyone',
+                                        'value': 'anyone',
+                                        'role': 'reader'})
+    return gfile
+# %%
+
+
+def main():
+    try:
+        file = send_zip(work_dir)
+        output = f'''
+# Download data from Google Drive
+import gdown
+!mkdir ../data
+url = "https://drive.google.com/uc?export=download&id={file["id"]}"
+data = pd.read_csv(gdown.download(url, output="../data/{file["title"]}",  
+quiet=True), compression="zip")
+data.head()'''
+    # Place text on clipboard
+        pc.copy(output)
+        print("Snippet copied to clipboard")
+        print(file["id"], file["title"])
+
+    except:
+        print('Usage: python gdrive.py')
+        pass
+
+
+if __name__ == "__main__":
+    main()
+# %%
+# import tomli
+
+# secrets = "/Users/gm/Documents/python_scripts/gdrive/config/.secrets.toml"
+# with open(secrets, "rb") as f:
+#     secrets = tomli.load(f)
+
+# print(secrets)
+
+
+# # %%
+# secrets.keys()
+# %%
